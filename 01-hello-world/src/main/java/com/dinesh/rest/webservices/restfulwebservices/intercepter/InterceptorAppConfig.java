df8d08106ebb43087464677e61da4a683920806f
Dasaratharao <dinesh.dasaratharao@capgemini.com>
2020-11-13 12:33:11 -0600

    hello-world-with-docker added

---
+++ 01-hello-world/src/main/java/com/dinesh/rest/webservices/restfulwebservices/intercepter/InterceptorAppConfig.java
@@ -0,0 +1,17 @@
+package com.dinesh.rest.webservices.restfulwebservices.intercepter;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurationSupport;
+
+@Component
+public class InterceptorAppConfig extends WebMvcConfigurationSupport {
+	@Autowired
+	GeneralInterceptor generalInterceptor;
+
+	@Override
+	public void addInterceptors(InterceptorRegistry registry) {
+		registry.addInterceptor(generalInterceptor);
+	}
+}
