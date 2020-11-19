d82d41cd6737f6a2da3fe52a44432caa74d32c8e
Dasaratharao <dinesh.dasaratharao@capgemini.com>
2020-11-18 21:35:15 -0600

    unwanted code cleaned up

--- 02-todo-web-application-h2/target/classes/data.sql
+++
@@ -1,6 +0,0 @@
-insert into TODO
-values(10001, 'Learn Spring Boot', false, sysdate(), 'dinesh');
-insert into TODO
-values(10002, 'Learn Angular JS', false, sysdate(), 'dinesh');
-insert into TODO
-values(10003, 'Learn to Dance', false, sysdate(), 'dinesh');
